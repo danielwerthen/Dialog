6f62b22d21196fbe2c142f68b9ef1fda5e84a49c
Daniel Werthén <danielwerthen@gmail.com>
2012-04-22 18:18:55 +0200

    Prod

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 	, options = { auto_reconnect: true, native_parser: true }
 
 module.exports.connect = function (dbString) {
-	if (!dbString) dbString = 'mongodb://heroku:Pass09Word@staff.mongohq.com:10046/app3931072' || process.env.DB_CONNECTION;
+	if (!dbString || dbString === 'production') dbString = 'mongodb://heroku:Pass09Word@staff.mongohq.com:10046/app3931072' || process.env.DB_CONNECTION;
 	else if (dbString === 'TEST')
 		dbString = 'mongodb://vows:Pass09Word@flame.mongohq.com:27041/Dialog_test';
 	else if (dbString === 'X')
